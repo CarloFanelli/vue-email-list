b34c042a0316c5d223138d32548a89d8adc21080
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-09-20 15:25:18 +0200

    print in page 10 adress

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,22 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        frase : ''
+        emailAdress : [],
       }
     },
 
     mounted(){
+        for (let index = 0; index < 10; index++) {
 
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(response =>{
+
+                
+                console.log(response.data.response);
+                this.emailAdress.push(response.data.response);            
+        })
         }
+        console.log(this.emailAdress);
+    }
     }
   ).mount('#app')
